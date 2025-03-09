c43c118c0a8768e76cacf7fe301b5b701b0cb8cb
tuoreste <tuoreste@gmail.com>
2025-03-09 04:44:04 +0100

    Ex04 done: Assertion error, sys args & argv

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,4 +1,13 @@
 import sys
 
-for arg in sys.argv
-    print(arg)
+args = sys.argv
+
+if len(args) == 1:
+    print()
+elif len(args) > 2:
+    print(AssertionError("more than one argument is provided"))
+elif not set(args[1]) - set("0123456789") == set():
+    print(AssertionError("argument is not an integer"))
+else:
+    x = int(args[1])
+    print("I'm Even." if x % 2 == 0 else "I'm Odd.")
